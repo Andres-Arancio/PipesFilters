cba061ca3175089b9d116a808fa5ef006a69e6a9
Andres-Arancio <aarancio97@gmail.com>
2022-10-17 20:47:30 -0300

    Ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,22 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            string pathtopicture = @$"{Environment.CurrentDirectory}\luke.jpg";
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(pathtopicture);
+            IPicture finalpicture;
+            
+            FilterNegative filterNegative = new FilterNegative();
+            FilterGreyscale filterGreyscale = new FilterGreyscale();
+
+            PipeNull pipe3 = new PipeNull();
+            PipeSerial pipe2 = new PipeSerial(filterNegative,pipe3);
+            PipeSerial pipe1 = new PipeSerial(filterGreyscale,pipe2);
+            
+            finalpicture = pipe1.Send(picture);
+            provider.SavePicture(finalpicture, @$"{Environment.CurrentDirectory}\newluke.jpg");
+
         }
     }
 }
